e2d962c59dc124610374d6999ca32fe2d4e41379
Rejnszu <lukaszrejnsz@gmail.com>
2022-05-05 10:40:07 +0200

    Adding user messages

--- messenger.js
+++ messenger.js
@@ -5,6 +5,12 @@ const messengerFullSize = document.querySelector(".messenger-fullSize");
 const closeIcon = document.querySelector(".close-icon");
 const messageInput = document.querySelector(".message-input");
 const sendIcon = document.querySelector(".send-icon");
+const messengerMiddlePart = document.querySelector(
+  ".messenger-fullSize__middlePart"
+);
+const asistantMessageHour = document.querySelector(".hour");
+asistantMessageHour.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}`;
+
 messengerRolled.addEventListener("click", () =>
   messengerFullSize.classList.toggle("active")
 );
@@ -12,10 +18,34 @@ closeIcon.addEventListener("click", () =>
   messengerFullSize.classList.toggle("active")
 );
 
-messageInput.addEventListener("input", function () {
+function deactivateSendButton() {
+  sendIcon.classList.remove("active");
+  sendIcon.removeEventListener("click", newUserMessage);
+}
+
+function newUserMessage() {
+  const messageTemplate = document.querySelector(
+    "[data-user-message-template]"
+  );
+  let message = messageTemplate.content.cloneNode(true).children[0];
+  let messageText = message.querySelector("[data-user-message-text]");
+  let messageHour = message.querySelector("[data-user-message-hour]");
+
+  messageText.innerHTML = messageInput.value;
+  messageHour.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}`;
+
+  messengerMiddlePart.append(message);
+  messageInput.value = "";
+  deactivateSendButton();
+}
+
+function isThereAnyMessageHandler() {
   if (messageInput.value == "") {
-    sendIcon.classList.remove("active");
+    deactivateSendButton();
   } else {
     sendIcon.classList.add("active");
+    sendIcon.addEventListener("click", newUserMessage);
   }
-});
+}
+
+messageInput.addEventListener("input", isThereAnyMessageHandler);
